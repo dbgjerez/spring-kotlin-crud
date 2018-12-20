2e3ccf51a96c276263af630d729b5132282071ca
David Borrego <dborrego@atsistemas.com>
2018-12-20 09:43:29 +0100

    ini prokect

---
+++ src/main/kotlin/com/dbg/kotlin/SpringKotlinCrudApplication.kt
@@ -0,0 +1,12 @@
+package com.dbg.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinCrudApplication>(*args)
+}
+
